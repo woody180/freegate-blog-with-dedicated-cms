d013feb28964c7486194ea1eef60b263a12b051d
woody180 <woody180@rocketmail.com>
2023-07-06 13:09:40 +0400

    path articles from home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('home');
+        $model = new \App\Models\ArticleModel();
+        
+        return view('home', [
+            'articles' => $model->paginate(2),
+            'pager' => $model->pager->getPageCount() > 1 ? $model->pager->links() : NULL
+        ]);
     }
 }
