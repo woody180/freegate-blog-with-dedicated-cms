cfddcd00f5e7783935ed355d82c530996991b794
woody180 <woody180@rocketmail.com>
2023-07-06 13:09:08 +0400

    articles loop in home view

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,169 +8,18 @@
 
 
 <?= $this->section('content') ?>
-<div>
-    <div class="uk-card">
 
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=7" alt="something" />
-        </div>
+    <?php foreach ($articles as $article): ?>
+        <?= view('partials/card', ['article' => $article]) ?>
+    <?php endforeach; ?>
 
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=8" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=9" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=10" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=1" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=2" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=3" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
-
-
-<div>
-    <div class="uk-card">
-        <div class="uk-card-media-top">
-            <a href="article.html" class="absolute-link"></a>
-            <img src="https://picsum.photos/500/400?random=4" alt="something" />
-        </div>
-
-        <div class="uk-card-body uk-padding-remove-left uk-padding-remove-right">
-            <h3 class="uk-card-title">Media Top</h3>
-
-            <p class="uk-text-meta uk-text-italic">December 14, 2016</p>
-
-            <p class="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-        </div>
-    </div>
-</div>
 <?= $this->endSection() ?>
 
+
 <?= $this->section('pager') ?>
-    <?= $this->include('includes/pager') ?>
+    <?php if ($pager): ?>
+        <?= $pager ?>
+    <?php endif; ?>
 <?= $this->endSection() ?>
 
 
