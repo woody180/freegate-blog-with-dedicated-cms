e123b5f016a88eb35480f8a64bfcae1c0d354f46
woody180 <woody180@rocketmail.com>
2023-07-17 13:42:44 +0400

    featured articles in Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,6 +9,7 @@ class Home extends BaseController
         $model = new \App\Models\ArticleModel();
         
         return view('home', [
+            'featured' => $model->featured(),
             'articles' => $model->getAll(), // Default articles per pages - 20
             'pager' => $model->pager->getPageCount() > 1 ? $model->pager->links() : NULL
         ]);
