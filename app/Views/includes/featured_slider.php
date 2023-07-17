71ff9ca2a0408428dac1c9647bf1c6c85c611231
woody180 <woody180@rocketmail.com>
2023-07-17 13:43:08 +0400

    print featured articles in featured_slider include

--- app/Views/includes/featured_slider.php
+++ app/Views/includes/featured_slider.php
@@ -4,50 +4,27 @@
         <div class="uk-position-relative uk-visible-toggle" tabindex="-1" uk-slideshow="animation: push; ratio: 16:6">
 
             <ul class="uk-slideshow-items">
+                <?php foreach ($featured as $f_article): ?>
                 <li>
                     <div class="uk-position-relative uk-width-1-1 uk-height-1-1">
-                        <img src="https://picsum.photos/1600/420" alt="" uk-cover>
+                        <img src="<?= $f_article->article_banner ?>" alt="" uk-cover>
                         <div class="uk-position-center uk-position-small uk-text-center uk-light uk-position-z-index">
-                            <span class="uk-label uk-margin-bottom">DIY</span>
-                            <h1 class="uk-margin-remove">Wild animals preparing food <br> for his own parents</h1>
-                            <a href="article-with-sidebar.html" class="uk-button uk-button-default uk-margin-top">Continue reading</a>
+                            
+                            <div class="uk-flex uk-flex-column">
+                                <?php foreach (explode(",", $f_article->blog_category_title) as $i => $cat): ?>
+                                <div>
+                                    <a href="<?= base_url("blog/" . explode(",", $f_article->blog_category_url)[$i]) ?>" class="uk-label uk-margin-bottom"><?= $cat ?></a>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                            
+                            <h1 class="uk-margin-remove"><?= strtoupper($f_article->article_title) ?></h1>
+                            <a href="<?= base_url("blog/".explode(",", $f_article->blog_category_url)[0] . "/{$f_article->article_url}") ?>" class="uk-button uk-button-default uk-margin-top">Continue reading</a>
                         </div>
                         <div class="layer-mask"></div>
                     </div>
                 </li>
-                <li>
-                    <div class="uk-position-relative uk-width-1-1 uk-height-1-1">
-                        <img src="https://picsum.photos/1600/420" alt="" uk-cover>
-                        <div class="uk-position-center uk-position-small uk-text-center uk-light uk-position-z-index">
-                            <span class="uk-label uk-margin-bottom">DIY</span>
-                            <h1 class="uk-margin-remove">Wild animals preparing food <br> for his own parents</h1>
-                            <a href="article-with-sidebar.html" class="uk-button uk-button-default uk-margin-top">Continiu reading</a>
-                        </div>
-                        <div class="layer-mask"></div>
-                    </div>
-                </li>
-                <li>
-                    <div class="uk-position-relative uk-width-1-1 uk-height-1-1">
-                        <img src="https://picsum.photos/1600/420" alt="" uk-cover>
-                        <div class="uk-position-center uk-position-small uk-text-center uk-light uk-position-z-index">
-                            <span class="uk-label uk-margin-bottom">DIY</span>
-                            <h1 class="uk-margin-remove">Wild animals preparing food <br> for his own parents</h1>
-                            <a href="article-with-sidebar.html" class="uk-button uk-button-default uk-margin-top">Continiu reading</a>
-                        </div>
-                        <div class="layer-mask"></div>
-                    </div>
-                </li>
-                <li>
-                    <div class="uk-position-relative uk-width-1-1 uk-height-1-1">
-                        <img src="https://picsum.photos/1600/420" alt="" uk-cover>
-                        <div class="uk-position-center uk-position-small uk-text-center uk-light uk-position-z-index">
-                            <span class="uk-label uk-margin-bottom">DIY</span>
-                            <h1 class="uk-margin-remove">Wild animals preparing food <br> for his own parents</h1>
-                            <button class="uk-button uk-button-default uk-margin-top">Continiu reading</button>
-                        </div>
-                        <div class="layer-mask"></div>
-                    </div>
-                </li>
+                <?php endforeach; ?>
             </ul>
 
             <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
