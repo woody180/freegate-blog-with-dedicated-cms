90c0efc48bf4c24406d1ed337ce11e4b40e9fd84
lacrimosakh <lacrimosakh@gmail.com>
2023-08-01 10:29:31 +0400

    refactor article view

--- app/Views/article.php
+++ app/Views/article.php
@@ -1,12 +1,12 @@
 <?= $this->extend('layoutes/article') ?>
 
 <?= $this->section('banner') ?>
-<img src="<?= $article->article_banner ?>" alt="">
+    <img src="<?= $article->article_banner ?>" alt="<?= $article->article_title ?>">
 <?= $this->endSection() ?>
 
 
 <?= $this->section('content') ?>
 
-<?= $article->article_body ?>
+    <?= $article->article_body ?>
 
 <?= $this->endSection() ?>
